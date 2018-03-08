9dad56154992980948dbe2c58c4151f407f3bee6
muhamad43705 <muhamad43705@gmail.com>
2018-03-08 15:45:09 +0700

    upload

--- key/default-database-config.php
+++ key/default-database-config.php
@@ -4,10 +4,10 @@
 
 $database = 'pgsql';
 $port = '5432';
-$hostname = 'localhost';
-$dbname = 'turboly';
-$userid ='postgres';
-$password = '121509';
+$hostname = 'ec2-54-197-254-189.compute-1.amazonaws.com';
+$dbname = 'df5lk6nae291ls';
+$userid ='rtabfrephftkxt';
+$password = '09415d9918323003cd53232a6f28d72c967375173077916d0becd278fca82f28';
 
 /*
 $database = 'mysql';
