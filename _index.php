f3ded02c16be8482da797a158bc16e95badfdd0a
muhamad43705 <muhamad43705@gmail.com>
2018-03-08 23:16:23 +0700

    upload

--- _index.php
+++ _index.php
@@ -5,7 +5,7 @@ include '_include.php';
 if (isset ($_SESSION[$class->loginAdminSession]))
 	session_unset($_SESSION[$class->loginAdminSession]);
 	
-
+$class->setLog('masuk ya');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
